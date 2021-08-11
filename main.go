8de9912c2199761f7757b84f59d7b922613ee922
root <root@DESKTOP-MASE.localdomain>
2021-08-10 21:46:22 -0400

    added main.go and simple framework

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	response, err := http.Get("https://api.sleeper.app/v1/user/445264202878152704/leagues/nfl/2021")
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(responseData))
+
+}
