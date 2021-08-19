58711f99b778c94b874d4dbdba8deb41873a3d89
Andrew Mason <ajmason09@gmail.com>
2021-08-18 21:57:52 -0400

    added prettyprint and unmarshaler

--- main.go
+++ main.go
@@ -1,11 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 )
+
+type User struct {
+	Verification   interface{} `json:"verification"`
+	Username       string      `json:"username"`
+	UserID         string      `json:"user_id"`
+	Token          interface{} `json:"token"`
+	SummonerRegion interface{} `json:"summoner_region"`
+	SummonerName   interface{} `json:"summoner_name"`
+	Solicitable    interface{} `json:"solicitable"`
+	RealName       interface{} `json:"real_name"`
+	Phone          interface{} `json:"phone"`
+	Pending        interface{} `json:"pending"`
+	Notifications  interface{} `json:"notifications"`
+	IsBot          bool        `json:"is_bot"`
+	Email          interface{} `json:"email"`
+	DisplayName    string      `json:"display_name"`
+	Deleted        interface{} `json:"deleted"`
+	DataUpdated    interface{} `json:"data_updated"`
+	Currencies     interface{} `json:"currencies"`
+	Created        interface{} `json:"created"`
+	Cookies        interface{} `json:"cookies"`
+	Avatar         string      `json:"avatar"`
+}
 
 func main() {
 	resp, err := http.Get("https://api.sleeper.app/v1/user/445264202878152704")
@@ -13,10 +37,20 @@ func main() {
 		log.Fatalln(err)
 	}
 	defer resp.Body.Close()
+
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatalln(err)
 	}
+	var user User
+	if err := json.Unmarshal([]byte(body), &user); err != nil {
+		log.Fatalln(err)
+	}
 
-	fmt.Println(string(body))
+	fmt.Println(PrettyPrint(user))
+}
+
+func PrettyPrint(i interface{}) string {
+	s, _ := json.MarshalIndent(i, "", "\t")
+	return string(s)
 }
