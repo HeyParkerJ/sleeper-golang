e50237561479a7b4a19a2ff71e65cf9aa003e473
Andrew Mason <ajmason09@gmail.com>
2021-08-23 21:33:52 -0400

    updated struct and added interface for scoring and settings

--- main.go
+++ main.go
@@ -31,7 +31,50 @@ type User struct {
 	Avatar         string      `json:"avatar"`
 }
 
+type League struct {
+	TotalRosters          int    `json:"total_rosters"`
+	Status                string `json:"status"`
+	Sport                 string `json:"sport"`
+	Shard                 int    `json:"shard"`
+	Settings              interface{}
+	SeasonType            string      `json:"season_type"`
+	Season                string      `json:"season"`
+	ScoringSettings       interface{} `json:"scoring_settings"`
+	RosterPositions       []string    `json:"roster_positions"`
+	PreviousLeagueID      string      `json:"previous_league_id"`
+	Name                  string      `json:"name"`
+	Metadata              interface{} `json:"metadata"`
+	LoserBracketID        interface{} `json:"loser_bracket_id"`
+	LeagueID              string      `json:"league_id"`
+	LastTransactionID     int64       `json:"last_transaction_id"`
+	LastReadID            string      `json:"last_read_id"`
+	LastPinnedMessageID   interface{} `json:"last_pinned_message_id"`
+	LastMessageTime       int64       `json:"last_message_time"`
+	LastMessageTextMap    interface{} `json:"last_message_text_map"`
+	LastMessageID         string      `json:"last_message_id"`
+	LastMessageAttachment interface{} `json:"last_message_attachment"`
+	LastAuthorIsBot       bool        `json:"last_author_is_bot"`
+	LastAuthorID          string      `json:"last_author_id"`
+	LastAuthorDisplayName string      `json:"last_author_display_name"`
+	LastAuthorAvatar      interface{} `json:"last_author_avatar"`
+	GroupID               interface{} `json:"group_id"`
+	DraftID               string      `json:"draft_id"`
+	DisplayOrder          int         `json:"display_order"`
+	CompanyID             interface{} `json:"company_id"`
+	BracketID             interface{} `json:"bracket_id"`
+	Avatar                string      `json:"avatar"`
+}
+
 func main() {
+	GetLeague()
+}
+
+func PrettyPrint(i interface{}) string {
+	s, _ := json.MarshalIndent(i, "", "\t")
+	return string(s)
+}
+
+func GetUser() {
 	resp, err := http.Get("https://api.sleeper.app/v1/user/445264202878152704")
 	if err != nil {
 		log.Fatalln(err)
@@ -50,7 +93,23 @@ func main() {
 	fmt.Println(PrettyPrint(user))
 }
 
-func PrettyPrint(i interface{}) string {
-	s, _ := json.MarshalIndent(i, "", "\t")
-	return string(s)
+func GetLeague() {
+	resp, err := http.Get("https://api.sleeper.app/v1/user/445264202878152704/leagues/nfl/2021")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	var result []League
+	if err := json.Unmarshal([]byte(body), &result); err != nil {
+		log.Fatalln(err)
+	}
+	for _, v := range result {
+		fmt.Println(PrettyPrint(v.ScoringSettings))
+	}
+
 }
